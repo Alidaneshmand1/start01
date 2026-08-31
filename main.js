4bb71ae9fb5140aa375f0b9c5da398e8b7e25445
unknown <[200~alisahragard46604660@gmail.com>
2026-08-31 18:19:17 +0330

    problem

---
+++ main.js
@@ -0,0 +1,21 @@
+    const button = document.getElementById('button1')
+    button.addEventListener('click' ,function loadDoc (){
+         const demo = document.getElementById('demo')
+
+        const xhttp = new XMLHttpRequest()
+        xhttp.onload = function() {
+        document.getElementById("demo").innerHTML =
+        this.responseText;
+        }
+        xhttp.open("GET" , 'ajax_info.txt')
+        xhttp.send()}
+)
+//         function loadDoc() {
+//   const xhttp = new XMLHttpRequest();
+//   xhttp.onload = function() {
+//     document.getElementById("demo").innerHTML =
+//     this.responseText;
+//   }
+//   xhttp.open("GET", "ajax_info.txt");
+//   xhttp.send();
+// }	
